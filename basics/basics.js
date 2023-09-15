1fb277cbf63490f55cdca3c3aa90fddd0b62f775
Mohammad Yunus Khan <142077383+khan1095@users.noreply.github.com>
2023-09-15 15:20:41 +0000

    container config done

--- basics/basics.js
+++ basics/basics.js
@@ -1,1 +1,10 @@
-console.log("khan");
+const accountId=14453;
+let accountEmail="khan@gmail.com";
+var accountPassword="123456";
+
+// accountId=2;
+accountEmail="123@gamil.com"
+accountPassword='12453';
+console.log(accountId);
+console.table([accountEmail,accountPassword]);
+
